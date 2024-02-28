c4b4addaba27b2731ecd3b2927c00bd7b57beefb
3yed <122279300+3yed-61@users.noreply.github.com>
2024-02-28 23:36:54 +0330

    Update collector.php

--- collector.php
+++ collector.php
@@ -12,31 +12,30 @@ if ($content !== false) {
     // Remove everything after the @ symbol
     $content = preg_replace('/@.*/', '', $content);
 
-    function changeNameInVmessLink($tuicLink) {
-        $jsonPart = base64_decode(substr($vmessLink, strpos($tuicLink, '://') + 3));
-        $data = json_decode($jsonPart, true);
+    function changeNameInTuicLink($tuicLink) {
+        // Assuming that "ps" in tuic link represents the name
+        $pattern = '/ps=([^&]+)/';
+        preg_match($pattern, $tuicLink, $matches);
 
-        if ($data !== null && isset($data['ps'])) {
-            $newName = implode(' | ', array_slice(explode(' | ', $data['ps']), 0, 2)) . ' | 3YED';
-            $data['ps'] = $newName;
-            $newJsonPart = base64_encode(json_encode($data));
-
-            return substr_replace($tuicLink, $newJsonPart, strpos($tuicLink, '://') + 3);
+        if (!empty($matches[1])) {
+            $newName = implode(' | ', array_slice(explode(' | ', $matches[1]), 0, 2)) . ' | 3YED';
+            $newNameEncoded = urlencode($newName);
+            return str_replace($matches[0], 'ps=' . $newNameEncoded, $tuicLink);
         }
 
-        return $vmessLink;
+        return $tuicLink;
     }
 
     $contentLines = explode(PHP_EOL, $content);
     foreach ($contentLines as &$line) {
         if (strpos($line, 'tuic://') === 0) {
-            $line = changeNameInVmessLink($line);
+            $line = changeNameInTuicLink($line);
         }
     }
     unset($line);
 
     $headerSections = [
-        '#profile-title: base64:WUVEIHwgVk1FU1Pwn5Sx',
+        '#profile-title: base64:M1lFRCDik4IgfCBWTUVTUw==',
         '#profile-update-interval: 1',
         '#subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=2546249531',
         '#profile-web-page-url: https://github.com/3yed-61'
