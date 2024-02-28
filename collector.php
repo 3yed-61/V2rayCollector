4316a875b56d07ee02791d65d6b264e43c575ac4
3yed <122279300+3yed-61@users.noreply.github.com>
2024-02-29 00:06:20 +0330

    Update collector.php

--- collector.php
+++ collector.php
@@ -1,17 +1,20 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 $url = "https://raw.githubusercontent.com/yebekhe/TVC/main/subscriptions/xray/normal/tuic";
 $content = file_get_contents($url);
 
 if ($content !== false) {
-   
+    // حذف قسمت‌های مورد نظر
     $content = str_replace("#profile-title: base64:VFZDIHwgVFVJQw==", "#profile-title: base64:M1lFRCDik4IgfCBWTUVTUw==", $content);
     $content = str_replace("#profile-web-page-url: https://github.com/yebekhe/TelegramV2rayCollector", "#profile-web-page-url: https://github.com/nameless4pub", $content);
 
-    
-    file_put_contents("export/tuic", $content);
-    echo "Content successfully saved.";
+    // ذخیره محتوا در یک فایل
+    file_put_contents("export/tuic.txt", $content);
+    echo "محتوا با موفقیت ذخیره شد.";
 } else {
-    echo "Error fetching content.";
+    echo "خطا در دریافت محتوا.";
 }
 ?>
