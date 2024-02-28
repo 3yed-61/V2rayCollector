ebe18f3fdd728cf46070a0ac71ac6756ff117bdc
3yed <122279300+3yed-61@users.noreply.github.com>
2024-02-28 23:58:15 +0330

    Update collector.php

--- collector.php
+++ collector.php
@@ -1,55 +1,17 @@
 <?php
 
-$url = 'https://raw.githubusercontent.com/yebekhe/TVC/main/subscriptions/xray/normal/tuic';
-$filePath = 'sub/tuic';
-
+$url = "https://raw.githubusercontent.com/yebekhe/TVC/main/subscriptions/xray/normal/tuic";
 $content = file_get_contents($url);
 
 if ($content !== false) {
-    // Remove the unwanted header sections
-    $content = preg_replace('/#profile-title: base64:VFZDIHwgVk1FU1M=.*?#profile-web-page-url: https:\/\/github\.com\/yebekhe\/TelegramV2rayCollector/ms', '', $content);
+   
+    $content = str_replace("#profile-title: base64:VFZDIHwgVFVJQw==", "#profile-title: base64:M1lFRCDik4IgfCBWTUVTUw==", $content);
+    $content = str_replace("#profile-web-page-url: https://github.com/yebekhe/TelegramV2rayCollector", "#profile-web-page-url: https://github.com/nameless4pub", $content);
 
-    // Remove everything after the @ symbol
-    $content = preg_replace('/@.*/', '', $content);
-
-    function changeNameInTuicLink($tuicLink) {
-        // Extracting name from the link
-        $pattern = '/\|([^|]+)@TUICity[^|]+\|/';
-        preg_match($pattern, $tuicLink, $matches);
-
-        if (!empty($matches[1])) {
-            $nameToRemove = '|%20@' . $matches[1] . '%20|';
-            return str_replace($nameToRemove, '', $tuicLink);
-        }
-
-        return $tuicLink;
-    }
-
-    $contentLines = explode(PHP_EOL, $content);
-    foreach ($contentLines as &$line) {
-        if (strpos($line, 'tuic://') === 0) {
-            $line = changeNameInTuicLink($line);
-        }
-    }
-    unset($line);
-
-    $headerSections = [
-        '#profile-title: base64:M1lFRCDik4IgfCBWTUVTUw==',
-        '#profile-update-interval: 1',
-        '#subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=2546249531',
-        '#profile-web-page-url: https://github.com/nameless4pub'
-    ];
-
-    $content = implode(PHP_EOL, $headerSections) . PHP_EOL . implode(PHP_EOL, $contentLines);
-
-    $writeResult = file_put_contents($filePath, $content);
-
-    if ($writeResult !== false) {
-        echo 'Content successfully fetched and specified sections replaced. Content saved in ' . $filePath . '.';
-    } else {
-        echo 'Error saving content to the file.';
-    }
+    
+    file_put_contents("export/tuic.txt", $content);
+    echo "Content successfully saved.";
 } else {
-    echo 'Error fetching content from the link.';
+    echo "Error fetching content.";
 }
 ?>
