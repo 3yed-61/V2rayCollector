a488813752f6f34bb0eba9f588ef9098daf59155
3λΞĐ <3yed1361@gmail.com>
2024-08-12 22:42:56 +0330

    Update collector.php

--- collector.php
+++ collector.php
@@ -35,12 +35,13 @@ if ($content !== false) {
     }
     unset($line);
 
-    $headerSections = [
-    $warp = "//profile-title: base64:M867zp7EkCDwk4SC8JOGgyBWTUVTU+KtkO+4jw==\n";
-    $warp .= "//profile-update-interval: 1\n";
-    $warp .= "//subscription-userinfo: upload=5368709120; download=645097156608; total=955630223360; expire=1762677732\n";
-    $warp .= "//profile-web-page-url: https://github.com/3yed-61\n\n";
-    ];
+$warp = "//profile-title: base64:M867zp7EkCDwk4SC8JOGgyBWTUVTU+KtkO+4jw==\n";
+$warp .= "//profile-update-interval: 1\n";
+$warp .= "//subscription-userinfo: upload=5368709120; download=645097156608; total=955630223360; expire=1762677732\n";
+$warp .= "//profile-web-page-url: https://github.com/3yed-61\n\n";
+
+$headerSections = [$warp];
+
 
     $content = implode(PHP_EOL, $headerSections) . PHP_EOL . implode(PHP_EOL, $contentLines);
 
