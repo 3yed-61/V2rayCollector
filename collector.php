a58e4e9723b64dbd4947f11fd2c0a6376f0cd1fb
3λΞĐ <3yed1361@gmail.com>
2026-09-26 16:37:13 +0330

    Update collector.php

--- collector.php
+++ collector.php
@@ -17,7 +17,7 @@ if ($content !== false) {
         $data = json_decode($jsonPart, true);
 
         if ($data !== null && isset($data['ps'])) {
-            $newName = implode(' | ', array_slice(explode(' | ', $data['ps']), 0, 2)) . ' | ⭐3YED';
+            $newName = implode(' | ', array_slice(explode(' | ', $data['ps']), 0, 2)) . ' | 3λΞĐ';
             $data['ps'] = $newName;
             $newJsonPart = base64_encode(json_encode($data));
 
